f0cb0685fe5a2d17633a1fef7e57b7fb0e006521
Franckfer <francodel4to@gmail.com>
2021-08-04 22:27:47 -0300

    agrgado de

--- NeoTech/src/controllers/adminController.js
+++ NeoTech/src/controllers/adminController.js
@@ -1,3 +1,5 @@
+let db = require('../data/dataBase')
+
 module.exports = {
     panel: (req, res) => {
         res.render('admin/adminPanel', {title: 'NeoTech - Panel General'})
@@ -9,10 +11,10 @@ module.exports = {
         res.render("admin/admin-edit-account", {title: 'NeoTech - Editar Cuenta'})
     },
     formEditProducto: (req,res)=>{
-        res.render("admin/admin-edit-product-form", {title: 'NeoTech - Form Editar Producto'})
+        res.render("admin/admin-edit-product-form", {title: 'NeoTech - Form Editar Producto', db})
     },
     editProducto: (req,res)=>{
-        res.render("admin/admin-edit-product", {title: 'NeoTech - Editar Producto'})
+        res.render("admin/admin-edit-product", {title: 'NeoTech - Editar Producto', db})
     },
     ventaStock: (req,res)=>{
         res.render("admin/admin-sell-stock", {title: 'NeoTech - Ventas Y Stock'})
--- NeoTech/src/data/dataBase.js
+++ NeoTech/src/data/dataBase.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-let DBParseado = JSON.parse(fs.readFileSync('./data/articulos.json', 'utf-8'))
+let DBParseado = JSON.parse(fs.readFileSync('./src/data/articulos.json', 'utf-8'))
 
 
 
