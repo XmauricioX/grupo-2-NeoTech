08860b82afae64abf0335276965de2d981e3eec9
xmauriciox <alanmaneyro@hotmail.com>
2021-08-13 13:39:49 -0300

    rutas del sprint agregadas y comentario en admin router

--- NeoTech/src/routes/product.js
+++ NeoTech/src/routes/product.js
@@ -3,9 +3,11 @@ var router = express.Router();
 let controller = require('../controllers/productController')
 
 
-/* GET products page. */
+/* GET LISTADO DE PRODUCTOS. */
 router.get('/lista-de-productos', controller.productList)
+// GET 
 router.get("/lista-de-productos/:id", controller.productDetail)
+
 
 
 module.exports = router;
