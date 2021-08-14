aa8102c7e26a4de34ca01c2d2a070ae355170765
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-08-14 10:41:38 -0300

    crud: metodo delete, modificaciones en dataBase.js, entry point: descarga y configuracion de la dependencia method-override

--- NeoTech/src/data/dataBase.js
+++ NeoTech/src/data/dataBase.js
@@ -1,6 +1,9 @@
 let fs = require('fs')
 
-let DBParseado = JSON.parse(fs.readFileSync('./src/data/articulos.json', 'utf-8'))
-
-
-module.exports = DBParseado;
+module.exports = {
+    getProducts : JSON.parse(fs.readFileSync('./src/data/products.json', 'utf-8')),
+    getUsers : JSON.parse(fs.readFileSync('./src/data/users.json', 'utf-8')),
+    writeJSON : (dataBase) => {
+        fs.writeFileSync('./src/data/products.json', JSON.stringify(dataBase), "utf-8")
+    }
+}
