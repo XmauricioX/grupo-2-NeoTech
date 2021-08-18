0e93637ddfc55e17878e1f70dfa4fd9cabcd3b67
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-08-17 21:29:45 -0300

    nuevos cambios sin romper

--- NeoTech/src/controllers/productController.js
+++ NeoTech/src/controllers/productController.js
@@ -9,5 +9,17 @@ module.exports = {
             return productID.id === +req.params.id
         });
         res.render("products/productDetail", {title: 'NoeTech - Detalle de Producto', productID })
+    },
+    /* menu: (req, res) => {
+        let result = []
+        getProducts.forEach(element => {
+            let marcaFiltrada = getProducts.filter(marca)
+            if(marcaFiltrada === element.marca)
+            result.push(element)
+        });
+        res.redirect('producto/lista-de-productos', {
+            result,
+            products : getProducts
+        }) */
     }
-}
+
