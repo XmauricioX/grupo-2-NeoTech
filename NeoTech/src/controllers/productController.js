445284f3a8c8129b66d9ded3968b328beb1b08e8
Franckfer <francodel4to@gmail.com>
2021-08-25 00:38:24 -0300

    seleccion de categorias al editar productos arregladladod

--- NeoTech/src/controllers/productController.js
+++ NeoTech/src/controllers/productController.js
@@ -1,4 +1,4 @@
-let { getProducts} = require("../data/dataBase");
+let { getProducts } = require("../data/dataBase");
 
 module.exports = {
     productList: (req, res) => {
@@ -9,5 +9,19 @@ module.exports = {
             return productID.id === +req.params.id
         });
         res.render("products/productDetail", {title: 'NoeTech - Detalle de Producto', productID })
-    }
+    },
+
+    ///////////////////////////////////////////////////////////
+    productCategories: (req,res) =>{
+        let categoryId = req.params.category.trim()
+
+        let categoryFound = getProducts.filter(product => product.category == categoryId)
+        //de getProducts fitramos el producto cuya categoria sea igual a la recibida por parametro
+        res.render('products/productList', {
+            title: 'NeoTech - Lista de Productos',
+            category : categoryFound,
+            
+        })
+    },
+    ////////////////////////////////////////////////////////////////////
 }
