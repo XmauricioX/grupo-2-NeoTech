e4e1e52cf2cd8e471b98496b8792ec2bbeb71e04
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-08-26 19:41:34 -0300

    a

--- NeoTech/src/controllers/productController.js
+++ NeoTech/src/controllers/productController.js
@@ -2,28 +2,42 @@ let { getProducts } = require("../data/dataBase");
 
 module.exports = {
     productList: (req, res) => {
-        res.render('products/productList', {title: 'NeoTech - Lista de Productos', products: getProducts })
+        res.render('products/productList', { title: 'NeoTech - Lista de Productos', products: getProducts })
     },
     productDetail: (req, res) => {
-        let productID = getProducts.find(productID =>{
+        let productID = getProducts.find(productID => {
             return productID.id === +req.params.id
         });
-        res.render("products/productDetail", {title: 'NeoTech - Detalle de Producto', productID })
+        res.render("products/productDetail", {
+            title: 'NeoTech - Detalle de Producto',
+            productID
+        })
     },
     search: (req, res) => {
-       
-            let result = []
-            getProducts.forEach(element => {
-                if(element.description.toLowerCase().includes(req.query.keywords.toLowerCase())) {
-                    result.push(element)
-                }
-            });
-                res.render('products/results', {
-                    result,
-                    title: 'NeoTech - Resultado de busqueda',
-                    products: getProducts
-                })
+
+        let result = []
+        getProducts.forEach(element => {
+            if (element.description.toLowerCase().includes(req.query.keywords.toLowerCase())) {
+                result.push(element)
+            }
+        });
+        res.render('products/results', {
+            result,
+            title: 'NeoTech - Resultado de busqueda',
+            products: getProducts
+        })
+    },
+    category: (req, res) => {
+        let categoryparams = req.params.categoria
         
-        },
+        let category = getProducts.filter(product => product.category == categoryparams)
+        res.send(category)
+        
     }
+        
+    ,
+    trademark: (req, res) => {
+        
+    },
+}
 
--- NeoTech/src/routes/product.js
+++ NeoTech/src/routes/product.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
-let controller = require('../controllers/productController')
+let {productList,
+    productDetail,
+    search,
+    category,
+    trademark} = require('../controllers/productController')
 
 
 /* GET LISTADO DE PRODUCTOS. */
-router.get('/lista-de-productos', controller.productList)
-// GET 
-router.get("/lista-de-productos/:id", controller.productDetail)
-
-router.get("/resultado-de-busqueda", controller.search)
+router.get('/lista-de-productos', productList)
+// GET DETALLE DE PRODUCTO   
+router.get("/lista-de-productos/:id", productDetail)
+/* GET BUSCADOR */
+router.get("/resultado-de-busqueda", search)
+/* GET PRODUCTOS POR CATEGORIA */
+router.get('/categoria/:categoria', category)
+/* GET PRODUCTOS POR MARCA */
+router.get('/marca', trademark)
 
 
 module.exports = router;
