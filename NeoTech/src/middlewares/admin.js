d22e09a4b21fe81238468e9d3dc3d5285970b0b9
arielhandrade88 <arielhandrade@hotmail.com.ar>
2021-09-05 22:50:26 -0300

    arreglo middleware admin

--- NeoTech/src/middlewares/admin.js
+++ NeoTech/src/middlewares/admin.js
@@ -1,8 +1,8 @@
 module.exports = function admin(req, res, next) {
-    if (req.session.user.rol == false) {
-        return res.redirect("/")
+    if (req.session.user.rol == true) {
+        next()
     }
-    next()
+    res.redirect("/")
 }
 
 // si sos admin te deja pasar al panel sino, no te deja
