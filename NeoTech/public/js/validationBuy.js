254d0bdb3022f1ef29b46bfdb8bea8fa1860eee7
XmauricioX <alanmaneyro@hotmail.com>
2021-10-22 18:15:54 -0300

    buy agregado

--- NeoTech/public/js/validationBuy.js
+++ NeoTech/public/js/validationBuy.js
@@ -1,21 +1,29 @@
 window.addEventListener('load', function(){
-
+    
     let $number = document.querySelector("#number");
     let $numberErr = document.querySelector("#numberErr");
 
     let $code = document.querySelector('#code');
     let $codeErr = document.querySelector('#codeErr');
 
-    let $vencimiento = document.querySelector('#vencimiento');
-    let $vencimientoErr = document.querySelector('#vencimientoErr');
+    let $vencimientoMes = document.querySelector('#vencimientoMes');
+    let $vencimientoMesErr = document.querySelector('#vencimientoMesErr');
+
+    let $vencimientoAño = document.querySelector("#vencimientoAño");
+    let $venvimientoAñoErr = document.querySelector("#vencimientoAñoErr");
 
     let $titular = document.querySelector('#titular');
     let $titularErr = document.querySelector('#titularErr');
+
+    let $buy = document.querySelector("#buy");
 
     $number.addEventListener("blur", function() {
         switch (true) {
             case !$number.value.trim():
                 $numberErr.innerHTML = 'este campo es OBLIGATORIO'
+                break;
+            case $number.value.length != 16: 
+                $numberErr.innerHTML = 'deben ser 16 numeros'
                 break;
             default:
                 $numberErr.innerHTML = ''
@@ -27,18 +35,43 @@ window.addEventListener('load', function(){
             case !$code.value.trim():
                 $codeErr.innerHTML = 'este campo es OBLIGATORIO'
                 break;
+            case $code.value.length != 3:
+                $codeErr.innerHTML = 'deben ser 3 numeros'
+                break;
             default:
                 $codeErr.innerHTML = ''
                 break;
         }
     })
-    $vencimiento.addEventListener("blur", function() {
+    $vencimientoAño.addEventListener("blur", function(){
         switch (true) {
-            case !$vencimiento.value.trim():
-                $vencimientoErr.innerHTML = 'este campo es OBLIGATORIO'
+            case !$vencimientoAño.value.trim():
+                $venvimientoAñoErr.innerHTML = 'este campo es OBLIGATORIO'
+                break;
+            case $vencimientoAño.value.length != 4:
+                $venvimientoAñoErr.innerHTML = 'deben ser 4 numeros'
+                break;
+            case $vencimientoAño.value < 2021:
+                $venvimientoAñoErr.innerHTML = 'esta tarjeta ya vencio'
                 break;
             default:
-                $vencimientoErr.innerHTML = ''
+                $venvimientoAñoErr.innerHTML = ''
+                break;
+        }
+    })
+    $vencimientoMes.addEventListener("blur", function() {
+        switch (true) {
+            case !$vencimientoMes.value.trim():
+                $vencimientoMesErr.innerHTML = 'este campo es OBLIGATORIO'
+                break;
+            case $vencimientoMes.value.length != 2:
+                $vencimientoMesErr.innerHTML = 'deben ser 2 numeros'
+                break;
+            case $vencimientoMes.value > 12:
+                $vencimientoMesErr.innerHTML = 'no hay mas de 12 meses :p'
+                break;
+            default:
+                $vencimientoMesErr.innerHTML = ''
                 break;
         }
     })
@@ -47,9 +80,15 @@ window.addEventListener('load', function(){
             case !$titular.value.trim():
                 $titularErr.innerHTML = 'este campo es OBLIGATORIO'
                 break;
+            case $titular.value.length < 2:
+                $titularErr.innerHTML = 'debe tener minimo 2 caracteres'
+                break;
             default:
                 $titularErr.innerHTML = ''
                 break;
         }
+    })
+    $buy.addEventListener("click", function(){
+        alert("Gracias por su compra")
     })
 })
