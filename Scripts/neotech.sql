2fb8969efad173c38fb4d2c6697723a60770b247
Franckfer <francodel4to@gmail.com>
2021-10-25 20:51:10 -0300

    CREATE DATABASE  IF NOT EXISTS neotech

--- Scripts/neotech.sql
+++ Scripts/neotech.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `neotech` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `neotech`;
 -- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
 -- Host: localhost    Database: neotech
