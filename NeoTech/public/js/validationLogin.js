104181ca2ca3f2bcbd7daf43812f87943330871f
XmauricioX <alanmaneyro@hotmail.com>
2021-10-28 18:48:23 -0300

    conflictos arreglados

--- NeoTech/public/js/validationLogin.js
+++ NeoTech/public/js/validationLogin.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-window.addEventListener('load', function(){
-
-    let $email = document.querySelector("#email")
-    let $emailErr = document.querySelector("#emailErr")
-
-    let $password = document.querySelector("#password")
-    let $passwordErr = document.querySelector("#passwordErr")
-
-    $email.addEventListener("blur", function() {
-        switch (true) {
-            case !$email.value.trim():
-                $emailErr.innerHTML = 'El campo email es OBLIGATORIO'
-                break;
-            default:
-                $emailErr.innerHTML = ''
-                break;
-        }
-    })
-    $password.addEventListener("blur", function () {
-        switch (true) {
-            case !$password.value.trim():
-                $passwordErr.innerHTML = 'La contraseña es OBLIGATORIA'
-                break;
-            default:
-                $passwordErr.innerHTML = ''
-=======
 function qs(element) {
     return document.querySelector(element)
 }
@@ -67,13 +40,10 @@ window.addEventListener('load', function () {
                 break;
             default:
                 $passwordErrors.innerHTML = ""
->>>>>>> 963263a666874aa8ba877983f886cee899371201
                 break;
         }
     })
 
-<<<<<<< HEAD
-=======
 
     //form
     $loginForm.addEventListener('submit', (event) => {
@@ -94,5 +64,4 @@ window.addEventListener('load', function () {
             $loginForm.submit()
         }
     })
->>>>>>> 963263a666874aa8ba877983f886cee899371201
 })
--- NeoTech/public/js/validationRegister.js
+++ NeoTech/public/js/validationRegister.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-
-window.addEventListener("load", function () {
-
-    let $inputName = document.querySelector("#firstName");
-    let $small1 = document.querySelector("#small-first-name")
-
-    let $inputLastName = document.querySelector("#lastName")
-    let $small2 = document.querySelector("#small-last-name")
-
-    let $inputEmail = document.querySelector("#email")
-    let $small3 = document.querySelector("#small-email")
-
-    let $pass1 = document.querySelector("#password")
-    let $errPass1 = document.querySelector("#small-password")
-
-    let $pass2 = document.querySelector("#password2")
-    let $errPass2 = document.querySelector("#small-password-2")
-
-    let $check = document.querySelector("#check")
-    let $checkErr = document.querySelector("#small-check");
-
-    let regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
-     regExDNI = /^[0-9]{7,8}$/,
-     regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-     regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
-
-
-    $inputName.addEventListener("blur", function () {
-        switch (true) {
-            case !$inputName.value.trim():
-                $small1.innerHTML = 'El campo nombre es OBLIGATORIO'
-                break;
-            case $inputName.value.length < 2:
-                $small1.innerHTML = 'debe tener almenos 2 caracteres'
-                break;
-            default:
-                $small1.innerHTML = ''
-=======
 function qs(element) {
     return document.querySelector(element)
 }
@@ -83,23 +44,10 @@ window.addEventListener("load", function () {
                 break;  
             default:
                 $inputNameErrors.innerHTML = ''
->>>>>>> 963263a666874aa8ba877983f886cee899371201
                 break;
         }
     })
 
-<<<<<<< HEAD
-    $inputLastName.addEventListener("blur", function () {
-        switch (true) {
-            case !$inputLastName.value.trim():
-                $small2.innerHTML = "El campo apellido es OBLIGATORIO"
-                break;
-            case $inputLastName.value.length < 2:
-                $small2.innerHTML = "debe tener almenos 2 caracteres"
-                break;
-            default:
-                $small2.innerHTML = ""
-=======
 
     //lastname
     $inputLastName.addEventListener("blur", function () {
@@ -115,57 +63,10 @@ window.addEventListener("load", function () {
                 break;      
             default:
                 $inputLastNameErrors.innerHTML = ""
->>>>>>> 963263a666874aa8ba877983f886cee899371201
                 break;
         }
     })
 
-<<<<<<< HEAD
-    $inputEmail.addEventListener('blur', function() {
-        switch (true) {
-            case !$inputEmail.value.trim():
-                $small3.innerHTML = 'El campo email es OBLIGATORIO'
-                break;
-            case !regExEmail.test($inputEmail.value):
-                $small3.innerHTML = 'Debe ingresar un email válido'
-                break;    
-            default:
-                $small3.innerHTML = ''
-                break;
-        }
-    })
-    $pass1.addEventListener("blur", function () {
-        switch (true) {
-            case !$pass1.value.trim():
-                $errPass1.innerHTML = "El campo contraseña es obligatorio"
-                break;
-                case $pass1.value.length < 8:
-                    $errPass1.innerHTML = 'debe tener almenos 8 caracteres'
-                    break;
-            default:
-                $errPass1.innerHTML = ''
-                break;
-        }
-    })
-    $pass2.addEventListener("blur", function () {
-        switch (true) {
-            case !$pass2.value.trim():
-                $errPass2.innerHTML = 'Confirmar la contraseña es obligatorio'
-                break;
-            case $pass2.value.length < 8:
-                $errPass2.innerHTML = 'debe tener almenos 8 caracteres'
-                break;
-            default:
-                $errPass2.innerHTML = ''
-                break;
-        }
-    })
-    $check.addEventListener('change', function () {
-        this.checked ? $checkErr.innerHTML = "" : $checkErr.innerHTML = 'Aceptar los terminos es OBLIGATORIO';
-    });
-
-})
-=======
 
     //email
     $email.addEventListener('blur', function() {
@@ -263,4 +164,3 @@ window.addEventListener("load", function () {
     })
 
 })
->>>>>>> 963263a666874aa8ba877983f886cee899371201
