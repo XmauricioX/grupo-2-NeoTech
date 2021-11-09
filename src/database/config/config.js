cbec7a22969050d53dbbfb3f143b8e7fd4ef6c55
XmauricioX <alanmaneyro@hotmail.com>
2021-11-09 13:16:52 -0300

    agregado de database en production para prueba

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "wybybnvkspzoqn",
+    "password": "b3cd113261f3b6355fd7ae39e898630c75c8fbb08702acb2a94dee192e961525",
+    "database": "dcfajr621crmn8",
+    "host": "ec2-174-129-225-160.compute-1.amazonaws.com",
     "dialect": "mysql"
   }
 }
