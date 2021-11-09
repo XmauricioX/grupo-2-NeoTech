e5bb927ae33342a167bf7816fd4fa84c80162450
XmauricioX <alanmaneyro@hotmail.com>
2021-11-09 13:25:40 -0300

    probadno database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "neotech",
-    "host": "127.0.0.1",
+    "username": "wybybnvkspzoqn",
+    "password": "b3cd113261f3b6355fd7ae39e898630c75c8fbb08702acb2a94dee192e961525",
+    "database": "dcfajr621crmn8",
+    "host": "ec2-174-129-225-160.compute-1.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
